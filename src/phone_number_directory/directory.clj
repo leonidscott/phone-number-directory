3fbb75272138be8329541512bae3fc3d17a37ffe
morrislenny <scot0530@morris.umn.edu>
2019-10-21 21:55:42 -0400

    Add e164 check and groupBy :phone-number to `get-seed-data`

--- src/phone_number_directory/directory.clj
+++ src/phone_number_directory/directory.clj
@@ -3,19 +3,35 @@
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
+(def- directory (atom get-seed-data))
+
 (defn- csv-data->maps
-  "header is a vector of keywords
+  "header is a vector of keywords [:h0 ... :hn]
    [[line0], [line1], ... [line n]] ->
    ({:h0 line0val0, ..., :hn l0vn}, ..., {:h0 lnv0, ..., :hn lnvn})"
   [header csv-data]
   (map #(zipmap header %) csv-data))
 
-(defn get-seed-data
+(defn- strip
+  "strip <string-chars> from <string>"
+  [strip-chars string]
+  (apply str (remove #((set strip-chars) %) string)))
+
+(defn- e164-convert
+  [phone-number]
+  (let [stripped-phone-number (strip "()- " phone-number)]
+    (if (str/includes? stripped-phone-number "+")
+      stripped-phone-number
+      (str "+1" stripped-phone-number))))
+
+(defn- get-seed-data
   "Returns a map:
     {phone-number -> {:phone-number (E.164 Format), :context, :caller-id}}
    from file interview-callerid-data.csv.
-   ASSUMPTION: There is a file called interview-callerid-data.csv in the the resources directory."
+   ASSUMPTION: There is a file called interview-callerid-data.csv resources/"
   []
   (->> (io/reader "resources/interview-callerid-data.csv")
        csv/read-csv ;Slurp CSV [[line0], [line1], ..., [line n]]
-       (csv-data->maps [:phone-number :context :name])))
+       (csv-data->maps [:phone-number :context :name])
+       (map #(update % :phone-number e164-convert)) ;convert phone-numbers to e164
+       (group-by :phone-number)))
