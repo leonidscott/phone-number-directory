43e95614884f044926d44cf02839ff57f7c34c8e
morrislenny <scot0530@morris.umn.edu>
2019-10-21 20:52:19 -0400

    Start fn `get-seed-data`. Add functionality for: * slurping from csv * converting csv data to a list of maps

--- src/phone_number_directory/directory.clj
+++ src/phone_number_directory/directory.clj
@@ -3,3 +3,19 @@
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
+(defn- csv-data->maps
+  "header is a vector of keywords
+   [[line0], [line1], ... [line n]] ->
+   ({:h0 line0val0, ..., :hn l0vn}, ..., {:h0 lnv0, ..., :hn lnvn})"
+  [header csv-data]
+  (map #(zipmap header %) csv-data))
+
+(defn get-seed-data
+  "Returns a map:
+    {phone-number -> {:phone-number (E.164 Format), :context, :caller-id}}
+   from file interview-callerid-data.csv.
+   ASSUMPTION: There is a file called interview-callerid-data.csv in the the resources directory."
+  []
+  (->> (io/reader "resources/interview-callerid-data.csv")
+       csv/read-csv ;Slurp CSV [[line0], [line1], ..., [line n]]
+       (csv-data->maps [:phone-number :context :name])))
