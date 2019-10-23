b15d21a235fccabf78517183fe14f212c0609b26
morrislenny <scot0530@morris.umn.edu>
2019-10-23 18:12:45 -0400

    Work on helper functions for `insert-record!`

--- src/phone_number_directory/directory.clj
+++ src/phone_number_directory/directory.clj
@@ -17,3 +17,28 @@
    If the phone-number is not present, returns []"
   [phone-number]
   ((deref-atom directory) (e164/convert phone-number)))
+
+(defn- record-new?
+  [record]
+  (->> (:phone-number record)
+       @directory
+       not))
+
+(defn- identical-record?
+  [record]
+  (let [records (@directory (:phone-number record))]
+    (not-empty (filter #(= % record) records))))
+
+(defn- name-context-conflict?
+  [{phone-number1 :phone-number context1 :context name1 :name}
+   {phone-number2 :phone-number context2 :context name2 :name}]
+  (and (= phone-number1 phone-number2)
+       (= context1 context2)
+       (not= name1 name2)))
+
+(defn insert-record!
+  [{:keys [phone-number context name] :as record}]
+  (let [e164-pn (e164/convert phone-number)]
+    (cond
+      (record-new? e164-pn) (swap! directory assoc e164-pn (vec record))
+      )))
