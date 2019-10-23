ab36e8a81254c6cc880e211197c6aef5e33fe4cd
morrislenny <scot0530@morris.umn.edu>
2019-10-23 08:18:26 -0400

    Shift e164 check logic from `csv_import.clj` to new file `e164.clj`

--- src/phone_number_directory/directory.clj
+++ src/phone_number_directory/directory.clj
@@ -2,3 +2,9 @@
   (:require [phone-number-directory.csv-import :as csv]))
 
 (def ^:private directory (atom (csv/get-seed-data)))
+
+(defn phone-number->records
+  "Takes in a single phone-number (String),
+   Returns vector of maps, each representing a phone record
+   If the phone-number is not present, returns []"
+  [phone-number])
