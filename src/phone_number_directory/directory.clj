22d7d9b979d00a2f2e2d4a858a57728cf05fb27d
morrislenny <scot0530@morris.umn.edu>
2019-10-23 10:58:07 -0400

    Working on `phone-number->records` and its tests

--- src/phone_number_directory/directory.clj
+++ src/phone_number_directory/directory.clj
@@ -1,10 +1,13 @@
 (ns phone-number-directory.directory
-  (:require [phone-number-directory.csv-import :as csv]))
+  (:require [phone-number-directory.csv-import :as csv]
+            [phone-number-directory.e164 :as e164]))
 
 (def ^:private directory (atom (csv/get-seed-data)))
 
 (defn phone-number->records
   "Takes in a single phone-number (String),
-   Returns vector of maps, each representing a phone record
+   converts it to an e164 phone-number,
+   and returns vector of maps, each representing a phone record
    If the phone-number is not present, returns []"
-  [phone-number])
+  [phone-number]
+  (@directory (e164/convert phone-number)))
--- test/phone_number_directory/directory_test.clj
+++ test/phone_number_directory/directory_test.clj
@@ -1,3 +1,17 @@
 (ns phone-number-directory.directory-test
   (:use midje.sweet)
   (:require [phone-number-directory.directory :as dir]))
+
+(def directory-initial {"+11111111111" [{:phone-number "+11111111111"
+                                        :context "context0"
+                                        :name "name0"}
+                                       {:phone-number "+11111111111"
+                                        :context "context1"
+                                        :name "name1"}]})
+
+(fact "phone-number->record returns a vector of maps (each representing a phone record) that corresponds to the phone-number"
+      (with-redefs [dir/directory directory-initial]
+        ;(println @#'dir/directory)
+        (println (dir/phone-number->records "+11111111111"))
+        ;=> (directory-initial "+11111111111")
+        ))
