fd52e889b9622f7e0297769acc14dd24b04483f8
morrislenny <scot0530@morris.umn.edu>
2019-10-23 16:59:52 -0400

    Finish tests for `phone-number->records`, add `(deref-atom)` in `directory.clj` to accomidate testing

--- src/phone_number_directory/directory.clj
+++ src/phone_number_directory/directory.clj
@@ -4,10 +4,16 @@
 
 (def ^:private directory (atom (csv/get-seed-data)))
 
+(defn deref-atom
+  "For testing, the tests need to redirect any derefs of directory.
+   Midje cannot mock deref, but it can mock this function"
+  [atom]
+  @atom)
+
 (defn phone-number->records
   "Takes in a single phone-number (String),
    converts it to an e164 phone-number,
    and returns vector of maps, each representing a phone record
    If the phone-number is not present, returns []"
   [phone-number]
-  (@directory (e164/convert phone-number)))
+  ((deref-atom directory) (e164/convert phone-number)))
--- test/phone_number_directory/directory_test.clj
+++ test/phone_number_directory/directory_test.clj
@@ -11,7 +11,16 @@
 
 (fact "phone-number->record returns a vector of maps (each representing a phone record) that corresponds to the phone-number"
       (with-redefs [dir/directory directory-initial]
-        ;(println @#'dir/directory)
-        (println (dir/phone-number->records "+11111111111"))
-        ;=> (directory-initial "+11111111111")
-        ))
+        (dir/phone-number->records "+11111111111") => (directory-initial "+11111111111")
+        (provided (dir/deref-atom anything) => @#'dir/directory)))
+
+(fact "phone-number->record corrects conventional phone numbers to e164"
+      (with-redefs [dir/directory directory-initial]
+        (dir/phone-number->records "(111) 111-1111")
+          => (directory-initial "+11111111111")
+        (provided (dir/deref-atom anything) => @#'dir/directory)))
+
+(fact "phone-number->record returns nil when the record does not exist"
+      (with-redefs [dir/directory directory-initial]
+        (dir/phone-number->records "+22222222222") => nil
+        (provided (dir/deref-atom anything) => @#'dir/directory)))
