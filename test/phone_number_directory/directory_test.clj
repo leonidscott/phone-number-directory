b4551f96cfcebe718d0afee486d95d324c8fe920
morrislenny <scot0530@morris.umn.edu>
2019-10-27 14:35:16 -0400

    Add first dir/insert-record! tests and supporting infastricture in directory.clj

--- test/phone_number_directory/directory_test.clj
+++ test/phone_number_directory/directory_test.clj
@@ -9,18 +9,47 @@
                                         :context "context1"
                                         :name "name1"}]})
 
+;;;
+;;phone-number->records tests
+;;;
+
 (fact "phone-number->record returns a vector of maps (each representing a phone record) that corresponds to the phone-number"
       (with-redefs [dir/directory directory-initial]
         (dir/phone-number->records "+11111111111") => (directory-initial "+11111111111")
-        (provided (#'dir/deref-atom anything) => @#'dir/directory)))
+        (provided (#'dir/deref-directory) => @#'dir/directory)))
 
 (fact "phone-number->record corrects conventional phone numbers to e164"
       (with-redefs [dir/directory directory-initial]
         (dir/phone-number->records "(111) 111-1111")
           => (directory-initial "+11111111111")
-        (provided (#'dir/deref-atom anything) => @#'dir/directory)))
+        (provided (#'dir/deref-directory) => @#'dir/directory)))
 
 (fact "phone-number->record returns nil when the record does not exist"
       (with-redefs [dir/directory directory-initial]
         (dir/phone-number->records "+22222222222") => nil
-        (provided (#'dir/deref-atom anything) => @#'dir/directory)))
+        (provided (#'dir/deref-directory) => @#'dir/directory)))
+
+;;;
+;;insert-record! tests
+;;;
+
+(with-redefs [dir/directory {}] ;to avoid importing the csv file
+  (let [new-pn "+22222222222"
+        new-record {:phone-number new-pn
+                    :context "context2-0"
+                    :name "name2-0"}
+        test-atom (atom directory-initial)]
+    (with-state-changes [(before :facts (reset! test-atom directory-initial))]
+      (fact "When a record contains a previously unseen phone-number,
+           insert-record! will add a new key value pair to directory"
+            (do (dir/insert-record! new-record)
+                @test-atom) => (assoc directory-initial new-pn [new-record])
+            (provided (#'dir/deref-directory) => @test-atom
+                      (#'dir/swap-directory! assoc new-pn [new-record])
+                      => (swap! test-atom assoc new-pn [new-record])))
+      (fact "When a record contains a previously unseen phone-number,
+           insert-record! will return that same record"
+            (dir/insert-record! new-record) => new-record
+            (provided (#'dir/deref-directory) => @test-atom
+                      (#'dir/swap-directory! assoc new-pn [new-record])
+                      => (swap! test-atom assoc new-pn [new-record]))))))
